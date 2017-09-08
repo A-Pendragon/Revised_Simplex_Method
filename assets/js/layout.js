f33cae453469cec90fe17efa253513f6bab9ce2b
dalerugs <dulce_rugayan@yahoo.com>
2017-09-09 02:10:47 +0800

    Main Menu

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,1 +1,14 @@
-$('#splashScreen').fadeIn(2000);
+function startButton(){
+	 $("#mainMenu").hide();
+	 $("#Calculator").fadeIn();
+}
+
+function credits(){
+	 $("#mainMenu").hide();
+	 $("#Credits").fadeIn();
+}
+
+function backButton(id){
+	 $("#"+id).hide();
+	 $("#mainMenu").fadeIn();
+}
